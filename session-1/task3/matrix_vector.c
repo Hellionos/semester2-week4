7132c10ddbc64fd82cfe25ce9414403cc68e45a2
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-16 13:45:39 +0000

    kinda done matrix vector task3

--- session-1/task3/matrix_vector.c
+++ session-1/task3/matrix_vector.c
@@ -4,11 +4,29 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main( void ) {
     int n = 4;           
     float **a, *b, *c;
 
+    a = calloc(n,sizeof(float));
+    for(int k = 0;k<4;++k) {
+        a[k]=calloc(n,sizeof(float));
+    }
+    b = calloc(n,sizeof(float));
+    c = calloc(n,sizeof(float));
+
+   for (int k=0;k<4;++k) {
+      for (int n=0;n<4;++n) {
+         a[k][n]=1;
+      }
+      b[k]=1;
+      for (int n=0;n<4;++n) {
+         c[k]=b[k]*a[k][n];
+      }
+      printf(" %f ",c[k]);
+   }
     /*
     Dynamically allocate the matrix a and vectors b and c using size n
     Intialise the matrix a and vector b entries to 1. 
@@ -17,6 +35,8 @@ int main( void ) {
     Print your final answer
     Free the allocated memory
     */
-    
+    free(a);
+    free(b);
+    free(c);
     return 0;
  }
