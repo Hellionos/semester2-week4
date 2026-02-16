9f36ed2962c3209f20b5855ccd60d8e3835c0228
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-16 11:34:09 +0000

    seem to have done factorial

--- session-1/task1/factorial.c
+++ session-1/task1/factorial.c
@@ -6,11 +6,16 @@
  #include <stdio.h>
 
  int main( void ) {
-    int f[20];
+    long int f[20];
+    for( int k=0; k<1; ++k) {
+    f[k]=1;
+    printf("%f\n",(double)f[k]);
+    }
+    for( int k=1; k<20; ++k ) {
+    f[k] = f[k-1]*k;
+    printf("%f\n",(double)f[k]);
+     }
 
-    /*
-    Code to compute the factorial of each array index
-    Print your final answer
-    */
     return 0;
  }
+/*17 values stored as int*/
