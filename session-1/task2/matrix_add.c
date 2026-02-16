fb800509417a65354aa2a75966cbfb85d96b4d03
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-16 11:59:46 +0000

    done matrix add

--- session-1/task2/matrix_add.c
+++ session-1/task2/matrix_add.c
@@ -10,6 +10,16 @@
     float b[4][4];
     float c[4][4];
 
+    for (int k=0;k<4;++k) {
+      for (int n=0;n<4;++n) {
+         a[k][n]=1;
+         b[k][n]=1;
+         c[k][n]=a[k][n]+b[k][n];
+         printf(" %f ",c[k][n]);
+      }
+      printf("\n");
+    }
+
     /*
     Intialise the matrix a and b entries to 1. 
     Write code to compute the sum.
