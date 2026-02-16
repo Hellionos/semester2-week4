78b937ab9166627f5b96e9f108db7a721d624690
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-16 12:05:28 +0000

    done matrix vector i think

--- session-1/task2/matrix_vector.c
+++ session-1/task2/matrix_vector.c
@@ -10,6 +10,16 @@
     float b[4];
     float c[4];
 
+   for (int k=0;k<4;++k) {
+      for (int n=0;n<4;++n) {
+         a[k][n]=1;
+      }
+      b[k]=1;
+      for (int n=0;n<4;++n) {
+         c[k]=b[k]*a[k][n];
+      }
+      printf(" %f ",c[k]);
+   }
     /*
     Intialise the matrix a and vector b entries to 1. 
     Write code to compute the product.
@@ -17,4 +27,4 @@
     Print your final answer
     */
     return 0;
- }
+}
