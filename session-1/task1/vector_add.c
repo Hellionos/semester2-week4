6c53bf2413a1ac9d9bb43afa9d6d02046524fb66
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-16 11:42:01 +0000

    done vector add

--- session-1/task1/vector_add.c
+++ session-1/task1/vector_add.c
@@ -10,6 +10,9 @@
     float b[]={ 2.0,3.0,4.0,5.0,6.0 };
     float c[5];
 
-    for (int k =0; k<5;++k)
+for (int k =0; k<5;++k) {
+   c[k] = a[k]+b[k];
+   printf("%f\n",c[k]);
+}
     return 0;
  }
