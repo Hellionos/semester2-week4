1de3f4490d9525ed329447a5714616b8a4d2956e
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-20 09:44:36 +0000

    done substring wll need help to properly do it

--- session-2/task2/substring.c
+++ session-2/task2/substring.c
@@ -4,6 +4,28 @@
 
  // create a program to locate a substring "ump" within a given string "The quick brown fox jumped over the lazy dog"
 
+#include <stdio.h>
+#include <string.h>
+
+int main (void) {
+
+    char foxstring[] = "The quick brown fox jumped over the lazy dog";
+    char ump[] = "ump";
+    char *ptr = strstr(foxstring,ump);
+    char *myptr = strtok(foxstring,ump);
+    if (ptr!=NULL);
+        printf("%p\n",&ptr);
+        printf("%s\n",ptr);
+        for (int k=0;k<3;k++) {
+        
+            printf("%c",ptr[k]);
+        }
+        printf("\n%s\n",myptr);
+
+        
+    return 0;
+}
+
  /*  Hints:
  1. include the appropriate libraries
  2. create the main function
@@ -13,3 +35,9 @@
  6. print the answer appropriately as pointer, character and string
  */ 
 
+/*char myStr[] = "Learn C++ at W3schools";
+char * myPtr = strtok(myStr, " ");
+while(myPtr != NULL) {
+  cout << myPtr << "\n";
+  myPtr = strtok(NULL, " ");
+}*/
