c9a4d68f9996e268497d95c38e36a75537a6fe64
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-20 10:29:42 +0000

    not done task 4 at all

--- session-2/task4/scan_sum.c
+++ session-2/task4/scan_sum.c
@@ -4,9 +4,16 @@
 int main( void ) {
 
     // define suitable data
-
+    char input[4];
+    int a = input[1];
+    int b = input[2];
+    int sum = a+b;
+    int count = scanf("%4s",input);
     // use scanf to read from the terminal
     // print the output from scanf and the data values 
+    printf("Read %d value\n",count);
+    printf("%d\n",sum);
 
     return 0;
 }
+
