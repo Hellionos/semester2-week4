e6512c060acd05f68411bc1e384ad075cba12e86
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-20 08:58:53 +0000

    sone string ops task2 session2

--- session-2/task2/string_ops.c
+++ session-2/task2/string_ops.c
@@ -14,10 +14,13 @@ int main( void ) {
     // use printf to verify the following tests
 
     // use 'strcmp' to compare 2 strings - print the return value
-
+    int result1 = strcmp(str1,str2);
+    printf("%d\n",result1);
     // use 'strcat' to concatenate 2 strings - print the resulting string
-
+    strcat(str1,str2);
+    printf("%s\n",str1);
     // use 'strcpy' to copy a string into str3 - print string 3
-
+    strcpy(str3,str2);
+    printf("%s\n",str3);
     return 0;
 }
