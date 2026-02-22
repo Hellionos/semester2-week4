bbc4a4d8ed28b5470bf81e47021731ce89801475
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-22 19:57:51 +0000

    not done task4 still

--- session-2/task4/scan_sum.c
+++ session-2/task4/scan_sum.c
@@ -4,15 +4,20 @@
 int main( void ) {
 
     // define suitable data
-    char input[4];
-    int a = input[1];
-    int b = input[2];
-    int sum = a+b;
-    int count = scanf("%4s",input);
+    int a,b;
+    int matched = scanf(" %d %*c %d %*c", &a, &b);
+  
+    
+    if (matched != 2) {
+        fprintf(stderr, "Input did not match the expected form: e.g., 3 + 4 =\n");
+        return 1;
+    }
+
+    
     // use scanf to read from the terminal
     // print the output from scanf and the data values 
-    printf("Read %d value\n",count);
-    printf("%d\n",sum);
+
+    printf("Sum: %d\n", a + b);
 
     return 0;
 }
