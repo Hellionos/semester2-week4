c02f508d5c04df6343dadcc14b5dcd835b48e9db
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-22 14:31:16 +0000

    not done makestring

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Ethan West
+Student ID: 201990893
 */
 
 #include <stdio.h>
@@ -10,10 +10,18 @@ Student ID:
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
-
+    char input[100];
     // process the command-line data using appropriate string functions
-
-    printf("%s\n",buffer); // use only this single print statement in your submitted code
+    for( int k=1; k<2; ++k ) {
+        printf("%s-",argv[k]);
+    }
+    for( int k=2; k<argc-1; ++k ) {
+        printf("%s-",argv[k]);
+    }
+    for( int k=argc-1; k<argc; ++k ) {
+        printf("%s",argv[k]);
+    }
+    //printf("%s\n",buffer); // use only this single print statement in your submitted code
 
     return 0;
 }
