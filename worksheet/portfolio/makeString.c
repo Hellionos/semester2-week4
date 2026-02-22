2d518b3ceac5a0dbca85956635817b10ef5abf12
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-22 15:08:07 +0000

    better makestring

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -12,13 +12,11 @@ int main( int argc, char **argv ) {
     // define appropriate data to hold your answer
     char buffer[50] = "";
     char hyphen[] = "-";
+
     // process the command-line data using appropriate string functions
+
     for( int k=1; k<argc; ++k ) {
-        if (k == 1) {
-            strcat(buffer,argv[k]);
-            strcat(buffer,hyphen);
-        }
-        if (k>1 && k<argc-1) {
+        if (k>0 && k<argc-1) {
             strcat(buffer,argv[k]);
             strcat(buffer,hyphen);
         }
@@ -26,9 +24,11 @@ int main( int argc, char **argv ) {
             strcat(buffer,argv[k]);
         }
     }
+
     if (argc == 2) {
-        strcpy(buffer,argv[1]);
+        strcpy(buffer,argv[1]); //special case when printing only one argument
     }
     printf("%s\n",buffer); // use only this single print statement in your submitted code
+
     return 0;
 }
